35dbe4b1abdd80ab2b56fab2f0018b875b109ace
qtng <qt.nguyen@gmx.de>
2026-09-22 01:18:12 +0200

    Fix navigation title based on pathname

--- js/navigation.js
+++ js/navigation.js
@@ -1,6 +1,6 @@
 (function() {
     const navConfig = {
-        title:  (location.pathname.indexOf("/qatt/") > -1) ? " QATT.org" : "Chunom.org",
+        title:  (location.pathname.indexOf("/qatt/") > -1) ? " Chunom.org" : "Chunom.org",
         sections: [
             {
                 header: "Chữ Nôm",
